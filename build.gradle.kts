df6dfc31d77b4b015035395cdf536dbcd25fee43
bvfnbk <bvfnbk@web.de>
2022-05-02 14:34:10 +0200

    Test publish configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/bvfnbk/oskit")
             credentials {
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
